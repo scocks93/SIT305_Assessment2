44e5c8a3ec94c7aefa6a5b88e0e56f4c90585113
Steven Cocks <scocks@deakin.edu.au>
2020-05-23 13:51:32 +1000

    Overview layout functional, initial navigation implemented, required fragments created.

--- app/src/main/java/com/example/assessment2/OverviewActivity.java
+++ app/src/main/java/com/example/assessment2/OverviewActivity.java
@@ -6,18 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class OverviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("Budget Wiz");
+        setContentView(R.layout.activity_overview);
+        setTitle("Overview");
     }
 
-
-    public void startClick(View view) {
-        Intent intent = new Intent(this, OverviewActivity.class);
-        startActivity(intent);
-    }
 }
