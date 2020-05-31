4651fbbd1c5338fcf2d1508757ff082db19367db
Steven Cocks <scocks@deakin.edu.au>
2020-05-31 22:54:15 +1000

    Added logging in functionality for existing users, added registration functionality for new users

--- app/src/main/java/com/example/assessment2/MainActivity.java
+++ app/src/main/java/com/example/assessment2/MainActivity.java
@@ -5,16 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Budget Wiz");
-    }
 
+        final EditText usernameEditText = findViewById(R.id.usernameEditText);
+        final EditText passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+        Button regButton = findViewById(R.id.regButton);
+
+        db = new DatabaseHelper(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean result = db.fetchUser(usernameEditText.getText().toString(), passwordEditText.getText().toString());
+                if (result == true) {
+                    Toast.makeText(MainActivity.this, "Log in successful.", Toast.LENGTH_LONG).show();
+                    Intent overviewIntent = new Intent(MainActivity.this, OverviewActivity.class);
+                    startActivity(overviewIntent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "User not found.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
 
     public void startClick(View view) {
         Intent intent = new Intent(this, OverviewActivity.class);
---
+++ app/src/main/java/com/example/assessment2/RegisterActivity.java
@@ -0,0 +1,57 @@
+package com.example.assessment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText userEditText = findViewById(R.id.userEditText);
+        final EditText createPasswordEditText = findViewById(R.id.createPasswordEditText);
+        final EditText createPasswordEditText2 = findViewById(R.id.createPasswordEditText2);
+        Button registerButton = findViewById(R.id.registerButton);
+        db = new DatabaseHelper(this);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = userEditText.getText().toString().trim();
+                String password = createPasswordEditText.getText().toString().trim();
+                String confirmedPassword = createPasswordEditText2.getText().toString().trim();
+
+                if (password.equals(confirmedPassword)) {
+                    long result = db.insert(user, password);
+
+                    if (result > 0) {
+                        Toast.makeText(RegisterActivity.this, "Registration Successful.", Toast.LENGTH_LONG).show();
+                        Intent backIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                        startActivity(backIntent);
+                    }
+                    else {
+                        Toast.makeText(RegisterActivity.this, "Registration Error.", Toast.LENGTH_LONG).show();
+                    }
+                }
+                else {
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void cancelClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
