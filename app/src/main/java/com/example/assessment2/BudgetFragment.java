db608530dce34751fbbd9411334d0499ee63de0f
Steven Cocks <scocks@deakin.edu.au>
2020-06-03 22:04:17 +1000

    - Budget fragment: Added onClickListener to edit button that will update the allocated budget within the fragment - Budget fragment: Added shared preferences to budgetAllocated field

--- app/src/main/java/com/example/assessment2/BudgetFragment.java
+++ app/src/main/java/com/example/assessment2/BudgetFragment.java
@@ -1,5 +1,7 @@
 package com.example.assessment2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,59 +9,54 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BudgetFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BudgetFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView budgetAllocated;
+    EditText budgetEditAmount;
+    Button budgetEdit;
+    SharedPreferences sharedPref;
+    String SAVED_BUDGET;
+    String savedBudget;
+
 
     public BudgetFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BudgetFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BudgetFragment newInstance(String param1, String param2) {
-        BudgetFragment fragment = new BudgetFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle("Budget");
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_budget, container, false);
+        View view = inflater.inflate(R.layout.fragment_budget, container, false);
+
+        budgetAllocated = (TextView) view.findViewById(R.id.budgetAllocated);
+        budgetEditAmount = (EditText) view.findViewById(R.id.budgetEditAmount);
+        budgetEdit = (Button) view.findViewById(R.id.budgetEdit);
+
+        sharedPref = getActivity().getSharedPreferences("com.example.assessment2", Context.MODE_PRIVATE);
+        // Get the saved budget from shared preferences and append it to the budgetAllocated TextView
+        savedBudget = sharedPref.getString(SAVED_BUDGET, "failed");
+        budgetAllocated.setText("$" + savedBudget);
+
+        // Change the allocated budget to the entered amount on click of edit button
+        budgetEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newBudget = budgetEditAmount.getText().toString();
+                budgetAllocated.setText("$" + newBudget);
+            }
+        });
+
+        return view;
     }
 }
